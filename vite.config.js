542ee8780d70c111ba05e0f81a27de199df761f0
Kellan Guinn-Bailey <kjgb001@bucknell.edu>
2025-12-18 13:03:55 -0500

    Updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/Spotify-Autobiography-Timeline/"
+  base: "/Spotify-Autobiography-Timeline/",
+  build: {
+    outDir: "docs"
+  }
 });
